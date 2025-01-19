7e432f9bc284f7648dde6ba64fa312d8d267996f
amiraliamjadi <amirali.amjadi@gmail.com>
2025-01-19 09:40:15 +0000

    add sqlite and migrate with test sample example user

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,25 @@
 from flask import Flask
 from .extensions import db
 from .routes import main
+from flask_migrate import Migrate
 
 def create_app():
-    app = Flask(__name__)
 
-    # Load configuration
-    app.config.from_object("config.DevelopmentConfig")
+    app = Flask(__name__, template_folder="templates")
+
+    # Configuration for SQLite database
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///m_sqlite.db'
+    # Optional: Suppress warnings about SQLAlchemy's track modifications
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     # Initialize extensions like database
     db.init_app(app)
 
-    # Register Blueprints
+    # Initialize Migrate
+    migrate = Migrate(app, db)
+
+
+    # Register Blueprints for routes
     app.register_blueprint(main)
 
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, render_template
+from .extensions import db
+from app.models import User
 
 # Define a Blueprint
 main = Blueprint("main", __name__)
@@ -6,3 +8,17 @@ main = Blueprint("main", __name__)
 @main.route("/")
 def index():
     return render_template("index.html")
+
+# Example usage (in a route or other function)
+@main.route('/about')
+def about():
+    # Example: Create a new user
+    new_user = User(username='testuser3', email='test3@example.com', password='12345')
+    db.session.add(new_user)
+    db.session.commit()
+
+    # Example: Query users
+    users = User.query.all()
+    user_names = [user.username for user in users]
+
+    return f"Users: {user_names}"
