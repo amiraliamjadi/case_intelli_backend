5d48a202f10a09d28d85753423c5054a9d850d86
amiraliamjadi <amirali.amjadi@gmail.com>
2025-01-20 07:18:57 +0000

    clean template part and delete most of things also add test to start build the frontend

--- app/routes.py
+++ app/routes.py
@@ -9,7 +9,7 @@ main = Blueprint("main", __name__)
 
 @main.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("test.html")
 
 
 # Register routes for APIs using Flask-RESTful
