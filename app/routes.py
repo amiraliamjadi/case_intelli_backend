e6d891e8934214cbdb014f80b8b4e2e96fe49fd3
amiraliamjadi <amirali.amjadi@gmail.com>
2025-01-20 09:39:22 +0000

    add all pages and their route to be ready for develop

--- app/routes.py
+++ app/routes.py
@@ -9,7 +9,54 @@ main = Blueprint("main", __name__)
 
 @main.route("/")
 def index():
-    return render_template("test.html")
+    return render_template("index.html")
+
+@main.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+@main.route("/login")
+def login():
+    return render_template("login.html")
+
+@main.route("/search")
+def search():
+    return render_template("search.html")
+
+@main.route("/parts/<int:part_id>")
+def part(part_id):
+    return render_template("part.html")
+
+
+
+@main.route("/crud/categories")
+def categories():
+    return render_template("category_crud.html")
+
+@main.route("/crud/units")
+def units():
+    return render_template("unit_crud.html")
+
+@main.route("/crud/stores")
+def stores():
+    return render_template("store_crud.html")
+
+@main.route("/crud/storepositions")
+def store_positions():
+    return render_template("storeposition_crud.html")
+
+@main.route("/crud/parties")
+def parties():
+    return render_template("party_crud.html")
+
+@main.route("/crud/users")
+def users():
+    return render_template("user_crud.html")
+
+@main.route("/crud/parts")
+def parts():
+    return render_template("part_crud.html")
+
 
 
 # Register routes for APIs using Flask-RESTful
